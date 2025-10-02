7a0bbfa32a0e77693a942d7bb1ea0dc8a9d0784e
Ozgun <ozgun@example.com>
2025-10-02 17:47:35 +0300

    fix(auth): add next-auth type definitions to resolve build error

---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,17 @@
+﻿import 'next-auth';
+
+declare module 'next-auth' {
+  /**
+   * Extends the built-in `User` model from `next-auth`.
+   */
+  interface User {
+    id: string;
+  }
+
+  /**
+   * Extends the built-in `Session` model from `next-auth`.
+   */
+  interface Session {
+    user: User;
+  }
+}
